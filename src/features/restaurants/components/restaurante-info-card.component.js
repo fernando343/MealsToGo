479a98e9519fbd8afb29783ef8dba79c3f24f5fc
Fernando Lopez <fernandojalope343@gmail.com>
2021-03-30 12:13:21 -0400

    themes

--- src/features/restaurants/components/restaurante-info-card.component.js
+++ src/features/restaurants/components/restaurante-info-card.component.js
@@ -3,11 +3,19 @@ import { Text, StyleSheet } from "react-native";
 import { Card } from "react-native-paper";
 import styled from "styled-components";
 
+const RestaurantCard = styled(Card)`
+  backgroundColor: #fff
+`;
+
+const RestaurantCardCover = styled(Card.Cover)`
+  padding: 20px;
+  backgroundColor: #fff;
+`;
 
 const Title = styled.Text`
   padding: 16px;
   color: red;
-`
+`;
 
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
@@ -23,14 +31,10 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
   } = restaurant;
 
   return (
-    <Card elevation={5} style={styles.card}>
-      <Card.Cover key={name} style={styles.cover} source={{ uri: photos[0] }} />
+    <RestaurantCard elevation={5}>
+      <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
       <Title>{name}</Title>
-    </Card>
+    </RestaurantCard>
   );
 };
 
-const styles = StyleSheet.create({
-  card: { backgroundColor: "white" },
-  cover: { padding: 20, backgroundColor: "white" },
-});
