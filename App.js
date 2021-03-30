f26628b7668c6c302279c534702bbd4a986366f0
Fernando Lopez <fernandojalope343@gmail.com>
2021-03-30 15:36:29 -0400

    addres

--- App.js
+++ App.js
@@ -1,14 +1,33 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
-import {RestautantsScreen} from './src/features/restaurants/screens/restaurants.screen'
+import { ThemeProvider } from "styled-components/native";
+
+import { useFonts as useOswald, Oswald_400Regular } from '@expo-google-fonts/oswald';
+import { useFonts as useLato, Lato_400Regular } from '@expo-google-fonts/lato';
+
+import { theme } from "./src/infrastructure/theme/index";
+import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
 
 
 export default function App() {
+
+  let [oswaldLoaded] = useOswald({
+    Oswald_400Regular
+  })
+  let [latoLoaded] = useLato({
+    Lato_400Regular
+  })
+
+  if (!oswaldLoaded || !latoLoaded) {
+    return null
+  }
+
   return (
     <>
-    <RestautantsScreen />
+      <ThemeProvider theme={theme}>
+        <RestaurantsScreen />
+      </ThemeProvider>
       <ExpoStatusBar style="auto" />
     </>
   );
 }
-
