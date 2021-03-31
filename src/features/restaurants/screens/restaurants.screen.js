0c0733644a33295d62b69c957043481f19f6da84
Fernando Lopez <fernandojalope343@gmail.com>
2021-03-31 16:53:29 -0400

    screen restaurant

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -15,12 +15,18 @@ const SearchContainer = styled.View`
   padding: ${(props) => props.theme.space[3]};
 `;
 
+const RestaurantList = styled(FlatList).attrs({
+  contentContainerStyle:{
+    padding: 16 
+  }
+})``;
+
 export const RestaurantsScreen = () => (
   <SafeArea>
     <SearchContainer>
       <Searchbar />
     </SearchContainer>
-    <FlatList
+    <RestaurantList
       data={[{ name: 1 }, { name: 2 }, { name: 3 }, { name: 4 }, { name: 5 }, { name: 6 }, { name: 7 }]}
       renderItem={() =>
         <Spacer position="bottom" size="large">
