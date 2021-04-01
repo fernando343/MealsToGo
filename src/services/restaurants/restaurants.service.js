e5322056e35cad321ffc3eb535c1ff5108692d5c
Fernando Lopez <fernandojalope343@gmail.com>
2021-04-01 17:40:11 -0400

    restairante service

--- src/services/restaurants/restaurants.service.js
+++ src/services/restaurants/restaurants.service.js
@@ -1,7 +1,7 @@
-import { mocks, mockImages } from "./mock";
+import { mocks } from "./mock";
 import camelize from "camelize";
 
-export const restaurantsRequest = (location) => {
+export const restaurantsRequest = (location = "37.7749295,-122.4194155") => {
   return new Promise((resolve, reject) => {
     const mock = mocks[location];
     if (!mock) {
@@ -11,19 +11,15 @@ export const restaurantsRequest = (location) => {
   });
 };
 
-export const restaurantsTransform = ({ results = [] }) => {
-  const mappedResults = results.map((restaurant) => {
-    restaurant.photos = restaurant.photos.map((p) => {
-      return mockImages[Math.ceil(Math.random() * (mockImages.length - 1))];
-    });
-
-    return {
-      ...restaurant,
-      address: restaurant.vicinity,
-      isOpenNow: restaurant.opening_hours && restaurant.opening_hours.open_now,
-      isClosedTemporarily: restaurant.business_status === "CLOSED_TEMPORARILY",
-    };
-  });
-
-  return camelize(mappedResults);
+const restaurantsTransform = (result) => {
+  const newResult = camelize(result);
+  return newResult;
 };
+restaurantsRequest()
+  .then(restaurantsTransform)
+  .then((transformedResponse) => {
+    console.log(transformedResponse);
+  })
+  .catch((err) => {
+    console.log("error");
+  });
