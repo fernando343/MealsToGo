9c8a5e016112e24f7099435491c48e13fcdebb40
Fernando Lopez <fernandojalope343@gmail.com>
2021-04-02 12:10:31 -0400

    search

--- src/features/restaurants/components/search.component.js
+++ src/features/restaurants/components/search.component.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import styled from "styled-components/native";
 import { Searchbar } from "react-native-paper";
 import { LocationContext } from '../../../services/location/location.context';
@@ -9,21 +9,26 @@ const SearchContainer = styled.View`
 `;
 
 export const Search = () => {
-  const { keyword, search } = useContext(LocationContext);
-  const [searchKeyword, setSearchKeyword] = useState(keyword);
+    const { keyword, search } = useContext(LocationContext);
+    const [searchKeyword, setSearchKeyword] = useState(keyword);
 
-  return (
-    <SearchContainer>
-      <Searchbar
-        placeholder="Search for a location"
-        value={searchKeyword}
-        onSubmitEditing={() => {
-          search(searchKeyword);
-        }}
-        onChangeText={(text) => {
-          setSearchKeyword(text);
-        }}
-      />
-    </SearchContainer>
-  );
+
+    useEffect(() => {
+        search(searchKeyword)
+    }, [])
+
+    return (
+        < SearchContainer >
+            <Searchbar
+                placeholder="Search for a location"
+                value={searchKeyword}
+                onSubmitEditing={() => {
+                    search(searchKeyword);
+                }}
+                onChangeText={(text) => {
+                    setSearchKeyword(text);
+                }}
+            />
+        </SearchContainer >
+    );
 };
