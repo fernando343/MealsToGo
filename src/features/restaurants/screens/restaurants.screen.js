d64323e4e67d16d19381c9191797c28689ed9455
Fernando Lopez <fernandojalope343@gmail.com>
2021-04-01 20:21:33 -0400

    create Restaurant context hook

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,36 +1,41 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Searchbar } from "react-native-paper";
 import { StatusBar, SafeAreaView, FlatList } from "react-native";
 import styled from "styled-components/native";
 
-import {SafeArea} from '../../../components/utility/safe-area.component';
+import { SafeArea } from '../../../components/utility/safe-area.component';
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
 import { Spacer } from "../../../components/spacer/spacer.component";
+import { RestaurantsContext } from "../../../services/restaurants/restaurants.context";
 
 const SearchContainer = styled.View`
   padding: ${(props) => props.theme.space[3]};
 `;
 
 const RestaurantList = styled(FlatList).attrs({
-  contentContainerStyle:{
-    padding: 16 
+  contentContainerStyle: {
+    padding: 16
   }
 })``;
 
-export const RestaurantsScreen = () => (
-  <SafeArea>
-    <SearchContainer>
-      <Searchbar />
-    </SearchContainer>
-    <RestaurantList
-      data={[{ name: 1 }, { name: 2 }, { name: 3 }, { name: 4 }, { name: 5 }, { name: 6 }, { name: 7 }]}
-      renderItem={() =>
-        <Spacer position="bottom" size="large">
-          <RestaurantInfoCard />
-        </Spacer>
-      }
-      keyExtractor={(item) => item.name}
-      contentContainerStyle={{ padding: 16 }}
-    />
-  </SafeArea>
-);
+export const RestaurantsScreen = () => {
+  const  restaurantContext = useContext(RestaurantsContext)
+  console.log(restaurantContext);
+  return (
+    <SafeArea>
+      <SearchContainer>
+        <Searchbar />
+      </SearchContainer>
+      <RestaurantList
+        data={restaurantContext.restaurants}
+        renderItem={() =>
+          <Spacer position="bottom" size="large">
+            <RestaurantInfoCard />
+          </Spacer>
+        }
+        keyExtractor={(item) => item.name}
+        contentContainerStyle={{ padding: 16 }}
+      />
+    </SafeArea>
+  );
+}
