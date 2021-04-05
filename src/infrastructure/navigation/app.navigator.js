77fd970563783c9e2c5d6a5f56a08ccb0f4fda5b
Fernando Lopez <fernandojalope343@gmail.com>
2021-04-04 20:09:15 -0400

    noraml

--- src/infrastructure/navigation/app.navigator.js
+++ src/infrastructure/navigation/app.navigator.js
@@ -8,10 +8,6 @@ import { SafeArea } from "../../components/utility/safe-area.component";
 import { RestaurantsNavigator } from "./restaurants.navigator";
 import { MapScreen } from "../../features/map/screens/map.screen";
 
-import {SettingsScreen} from "../../features/restaurants/screens/settingsScreen"
-
-
-
 
 
 const Tab = createBottomTabNavigator();
@@ -49,7 +45,7 @@ export const AppNavigator = () => (
     >
       <Tab.Screen name="Restaurants" component={RestaurantsNavigator} />
       <Tab.Screen name="Map" component={MapScreen} />
-      <Tab.Screen name="Settings" component={SettingsScreen} />
+      <Tab.Screen name="Settings" component={Settings} />
     </Tab.Navigator>
   </NavigationContainer>
 );
