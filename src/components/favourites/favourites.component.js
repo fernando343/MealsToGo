85469511b384198a6f9413d4cea480629210fd2a
Fernando Lopez <fernandojalope343@gmail.com>
2021-04-04 23:29:03 -0400

    favorite component

--- src/components/favourites/favourites.component.js
+++ src/components/favourites/favourites.component.js
@@ -2,8 +2,8 @@ import React, { useContext } from "react";
 import styled from "styled-components/native";
 import { AntDesign } from "@expo/vector-icons";
 import { TouchableOpacity } from "react-native";
-
 import { FavouritesContext } from "../../services/favourites/favourites.context";
+
 
 const FavouriteButton = styled(TouchableOpacity)`
   position: absolute;
@@ -12,26 +12,18 @@ const FavouriteButton = styled(TouchableOpacity)`
   z-index: 9;
 `;
 
-export const Favourite = ({ restaurant }) => {
-  const { favourites, addToFavourites, removeFromFavourites } = useContext(
-    FavouritesContext
-  );
+export const Favourite = () => {
+    const { favourites, addToFavourites, removeFromFavourites } = useContext(FavouritesContext);
+    const isFavourite = favourites.find((r) => r.placeId === restaurant.placeId)
 
-  const isFavourite = favourites.find((r) => r.placeId === restaurant.placeId);
 
-  return (
-    <FavouriteButton
-      onPress={() =>
-        !isFavourite
-          ? addToFavourites(restaurant)
-          : removeFromFavourites(restaurant)
-      }
-    >
-      <AntDesign
-        name={isFavourite ? "heart" : "hearto"}
-        size={24}
-        color={isFavourite ? "red" : "white"}
-      />
-    </FavouriteButton>
-  );
+    return (
+        <FavouriteButton onPress={() => !isFavourite ? addToFavourites(restaurant) : removeFromFavourites(restaurant)}>
+            <AntDesign name={
+                isFavourite ? "heart" : "hearto"
+            }
+                size={24}
+                color={isFavourite ? "red" : "white"} />
+        </FavouriteButton>
+    );
 };
